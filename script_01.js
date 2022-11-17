2ba64e720f0ffcf3990beead9fa1f24c49f4074e
Peter <schmidt.multimedia@gmail.com>
2022-11-17 08:55:11 +0100

    Lösungsbeispiel / SRP

--- script_01.js
+++ script_01.js
@@ -15,4 +15,44 @@
 output(getSentence(["Ich","bin","Max"],"S"));
 output(getSentence(["Bist","du","Max"],"Q"));
 output(getSentence(["Ich","bin"],"E"));
-output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,"," habbe","Sie","die"],"Q"));
+output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+function getSentence(arr,op) {
+
+    const GAP = " ";
+    let punct = getPunct(op);
+    let str = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        if ( i != arr.length-1 ) {
+            str += arr[i] + GAP; 
+        } else {
+            str += arr[i] + punct; 
+        }
+    }
+
+    return str;
+}
+
+// Modul Punktuation
+// output(getPunct("S"));
+// output(getPunct("Q"));
+// output(getPunct("E"));
+// output(getPunct("#!2"));
+function getPunct(op){
+    switch (op) {
+        case "S":
+            return ".";
+        case "Q":
+            return "?";
+        case "E":
+            return "!";
+        default:
+            return "#Error";  // TBA
+    }
+}
+
+// Modul: Ausgabe in Konsole : Test
+// output("hi"); 
+function output(inputStr) {
+    console.log(inputStr);
+}
